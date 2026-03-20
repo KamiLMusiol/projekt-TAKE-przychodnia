4a91759f179878bdb4cdb6b246f418951b371e15
KamiLMusiol <kam05mus@gmail.com>
2026-03-20 21:29:07 +0100

    przygotowanie pod commitowanie

---
+++ kod/przychodnia_baza/src/main/java/przychodnia/baza/backend/PrzychodniaBazaApplication.java
@@ -0,0 +1,13 @@
+package przychodnia.baza.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzychodniaBazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrzychodniaBazaApplication.class, args);
+	}
+
+}
