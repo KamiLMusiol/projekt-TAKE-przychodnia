fa8e7423664260430413e1696048bcdc15e7a9fb
KamiLMusiol <kam05mus@gmail.com>
2026-03-20 22:53:49 +0100

    klonowable

--- oprocentowanie/oprocentowanie-1/src/main/java/com/example/demo/ExceptionFromURL.java
+++ oprocentowanie/oprocentowanie-1/src/main/java/com/example/demo/ExceptionFromURL.java
@@ -1,9 +1,10 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 
-
-
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ExceptionFromURL extends RuntimeException {
 	
 
